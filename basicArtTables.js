dad93af1149f57d93a6a75b1228dce53f31004bd
Rob StClair <rob.stclair.57@gmail.com>
2018-06-08 14:55:12 -0400

    Update to using tablemanager.

--- basicArtTables.js
+++ basicArtTables.js
@@ -1,55 +1,115 @@
-const artDescriptionsData = [
-  ["artDescs1", ["silver ewer", "carved bone statuette", "carved ivory statuette", "finely wrought small gold bracelet"]],
-  ["artDescs2", ["cloth of gold vestments", "black velvet mask with numerous citrines", "silver chalice with lapis lazuli gems"]],
-  ["artDescs3", ["large well-made wool tapestry", "brass mug with jade inlays"]],
-  ["artDescs4", ["silver comb with moonstones", "silver-plated steel longsword with jet jewel in hilt"]],
-  ["artDescs5", ["carved harp of exotic wood with ivory inlay and zircon gems", "solid gold idol (10 lb.)"]],
-  ["artDescs6", ["gold dragon comb with red garnet eye", "gold and topaz bottle stopper cork", "ceremonial electrum dagger with a star ruby in the pommel"]],
-  ["artDescs7", ["eyepatch with mock eye of sapphire and moonstone", "fire opal pendant on a fine gold chain", "old masterpiece painting"]],
-  ["artDescs8", ["embroidered silk and velvet mantle with numerous moonstones", "sapphire pendant on gold chain"]],
-  ["artDescs9", ["embroidered and bejeweled glove", "jeweled anklet", "gold music box"]],
-  ["artDescs10", ["golden circlet with four aquamarines", "a string of small pink pearls (necklace)"]],
-  ["artDescs11", ["jeweled gold crown", "jeweled electrum ring"]],
-  ["artDescs12", ["gold and ruby ring", "gold cup set with emeralds"]]
-];
+const artDescs1 = {
+  "name": "artDescs1",
+  "showplayers": false,
+  "simpleEntries": ["silver ewer", "carved bone statuette", "carved ivory statuette", "finely wrought small gold bracelet"]
+};
+  
+const artDescs2 = {
+  "name":  "artDescs2",
+  "showplayers": false,
+  "simpleEntries": ["cloth of gold vestments", "black velvet mask with numerous citrines", "silver chalice with lapis lazuli gems"]
+};
 
-const artData = [
-  [1,10,   "[[1t[artDescs1]]] ([[1d10*10]] gp)"],
-  [11,25,  "[[1t[artDescs2]]] ([[3d6*10]] gp)"],
-  [26,40,  "[[1t[artDescs3]]] ([[1d6*100]] gp)"],
-  [41,50,  "[[1t[artDescs4]]] ([[1d10*100]] gp)"],
-  [51,60,  "[[1t[artDescs5]]] ([[2d6*100]] gp)"],
-  [61,70,  "[[1t[artDescs6]]] ([[3d6*100]] gp)"],
-  [71,80,  "[[1t[artDescs7]]] ([[4d6*100]] gp)"],
-  [81,85,  "[[1t[artDescs8]]] ([[5d6*100]] gp)"],
-  [86,90,  "[[1t[artDescs9]]] ([[1d4*1000]] gp)"],
-  [91,95,  "[[1t[artDescs10]]] ([[1d6*1000]] gp)"],
-  [96,99,  "[[1t[artDescs11]]] ([[2d4*1000]] gp)"],
-  [100,100,"[[1t[artDescs12]]] ([[2d6*1000]] gp)"]
-];
+const artDescs3 = {
+  "name":  "artDescs3",
+  "showplayers": false,
+  "simpleEntries": ["large well-made wool tapestry", "brass mug with jade inlays"]
+};
+
+const artDescs4 = {
+  "name":  "artDescs4",
+  "showplayers": false,
+  "simpleEntries": ["silver comb with moonstones", "silver-plated steel longsword with jet jewel in hilt"]
+};
+
+const artDescs5 = {
+  "name":  "artDescs5",
+  "showplayers": false,
+  "simpleEntries": ["carved harp of exotic wood with ivory inlay and zircon gems", "solid gold idol (10 lb.)"]
+};
+
+const artDescs6 = {
+  "name":  "artDescs6",
+  "showplayers": false,
+  "simpleEntries": ["gold dragon comb with red garnet eye", "gold and topaz bottle stopper cork", "ceremonial electrum dagger with a star ruby in the pommel"]
+};
+
+const artDescs7 = {
+  "name":  "artDescs7",
+  "showplayers": false,
+  "simpleEntries": ["eyepatch with mock eye of sapphire and moonstone", "fire opal pendant on a fine gold chain", "old masterpiece painting"]
+};
+
+const artDescs8 = {
+  "name":  "artDescs8",
+  "showplayers": false,
+  "simpleEntries": ["embroidered silk and velvet mantle with numerous moonstones", "sapphire pendant on gold chain"]
+};
+
+const artDescs9 = {
+  "name":  "artDescs9",
+  "showplayers": false,
+  "simpleEntries": ["embroidered and bejeweled glove", "jeweled anklet", "gold music box"]
+};
+
+const artDescs10 = {
+  "name":  "artDescs10",
+  "showplayers": false,
+  "simpleEntries": ["golden circlet with four aquamarines", "a string of small pink pearls (necklace)"]
+};
+
+const artDescs11 = {
+  "name":  "artDescs11",
+  "showplayers": false,
+  "simpleEntries": ["jeweled gold crown", "jeweled electrum ring"]
+};
+
+const artDescs12 = {
+  "name":  "artDescs12",
+  "showplayers": false,
+  "simpleEntries": ["gold and ruby ring", "gold cup set with emeralds"]
+};
+
+
+const basicArtItems = {
+  "name": "basicArtObjects",
+  "showplayers": false,
+  "entries": [
+    [1,10,"[[1t[artDescs1]]] ([[1d10*10]] gp)"],
+    [11,25,"[[1t[artDescs2]]] ([[3d6*10]] gp)"],
+    [26,40,"[[1t[artDescs3]]] ([[1d6*100]] gp)"],
+    [41,50,"[[1t[artDescs4]]] ([[1d10*100]] gp)"],
+    [51,60,"[[1t[artDescs5]]] ([[2d6*100]] gp)"],
+    [61,70,"[[1t[artDescs6]]] ([[3d6*100]] gp)"],
+    [71,80,"[[1t[artDescs7]]] ([[4d6*100]] gp)"],
+    [81,85,"[[1t[artDescs8]]] ([[5d6*100]] gp)"],
+    [86,90,"[[1t[artDescs9]]] ([[1d4*1000]] gp)"],
+    [91,95,"[[1t[artDescs10]]] ([[1d6*1000]] gp)"],
+    [96,99,"[[1t[artDescs11]]] ([[2d4*1000]] gp)"],
+    [100,100,"[[1t[artDescs12]]] ([[2d6*1000]] gp)"]
+  ]
+};
 
 
 on("ready", function() {
-  var tableName, artDescTable, artValuesTable, minRoll, maxRoll, valueAndDescriptionRoll, weight;
-  
-  _.each(artDescriptionsData, function(artDescLine) {
-    FindOrMakeObjector.setMode("rollabletable");
-    tableName = artDescLine.shift();
-    artDescTable = FindOrMakeObjector.findOrMake({"name": tableName, "showplayers": false});
-    FindOrMakeObjector.setMode("tableitem");
-    _.each(artDescLine.shift(), function(artDesc) {
-      FindOrMakeObjector.findOrMake({"name": artDesc, "_rollabletableid": artDescTable.id});
-    });
+  var artTables = [
+    artDescs1,
+    artDescs2,
+    artDescs3,
+    artDescs4,
+    artDescs5,
+    artDescs6,
+    artDescs7,
+    artDescs8,
+    artDescs9,
+    artDescs10,
+    artDescs11,
+    artDescs12,
+    basicArtItems
+  ];
+  _.each(artTables, function(tableData) {
+    RollableTableManager.findOrMake(tableData);
   });
-
-  FindOrMakeObjector.setMode("rollabletable");
-  artValuesTable = FindOrMakeObjector.findOrMake({"name": "artObjects", "showplayers": false});
-  FindOrMakeObjector.setMode("tableitem");
-  _.each(artData, function(artDataLine) {
-    [minRoll, maxRoll, valueAndDescriptionRoll] = artDataLine;
-    weight = (maxRoll - minRoll) + 1;
-    FindOrMakeObjector.findOrMake({"name": valueAndDescriptionRoll, "weight": weight, "_rollabletableid": artValuesTable.id});
-  })
 });
 
 
--- basicGemTables.js
+++ basicGemTables.js
@@ -1,44 +1,66 @@
-const gemDescriptionsData = [
-  ["gemDescs1", ["banded agate", "eye agate", "moss agate", "azurite", "blue quartz", "hematite", "lapis lazuli", "malachite", "obsidian", "rhodochrosite", "tiger eye turquoise", "freshwater (irregular) pearl"]],
-  ["gemDescs2", ["bloodstone", "carnelian", "chalcedony", "chrysoprase", "citrine", "iolite", "jasper", "moonstone", "onyx", "peridot", "rock crystal (clear quartz)", "sard", "sardonyx", "rose quartz", "smoky quartz", "star rose quartz", "zircon"]],
-  ["gemDescs3", ["amber", "amethyst", "chrysoberyl", "coral", "red garnet", "brown-green garnet", "jade", "jet", "white pearl", "golden pearl", "pink pearl", "silver pearl", "red spinel", "brown spinel", "deep green spinel", "tourmaline"]],
-  ["gemDescs4", ["alexandrite", "aquamarine", "violet garnet", "black pearl", "deep blue spinel", "golden yellow topaz"]],
-  ["gemDescs5", ["emerald", "white opal", "black opal", "fire opal", "blue sapphire", "fiery yellow corundum", "rich purple corundum", "blue star sapphire", "black star sapphire", "star ruby"]],
-  ["gemDescs6", ["clearest bright green emerald", "blue diamond", "white diamond", "canary diamond", "pink diamond", "brown diamond", "blue diamond", "jacinth"]]
-]
+const gemDescs1 = {
+  "name": "gemDescs1",
+  "showplayers": false,
+  "simpleEntries": ["banded agate", "eye agate", "moss agate", "azurite", "blue quartz", "hematite", "lapis lazuli", "malachite", "obsidian", "rhodochrosite", "tiger eye turquoise", "freshwater (irregular) pearl"]
+};
 
+const gemDescs2 = {
+  "name": "gemDescs2",
+  "showplayers": false,
+  "simpleEntries": ["bloodstone", "carnelian", "chalcedony", "chrysoprase", "citrine", "iolite", "jasper", "moonstone", "onyx", "peridot", "rock crystal (clear quartz)", "sard", "sardonyx", "rose quartz", "smoky quartz", "star rose quartz", "zircon"]
+};
 
-const gemData = [
-  [1,25,"[[1t[gemDescs1]]] ([[4d4]] gp)"],
-  [26,50,"[[1t[gemDescs2]]] ([[2d4*10]] gp)"],
-  [51,70,"[[1t[gemDescs3]]] ([[4d4*10]] gp)"],
-  [71,90,"[[1t[gemDescs4]]] ([[2d4*100]] gp)"],
-  [91,99,"[[1t[gemDescs5]]] ([[4d4*100]] gp)"],
-  [100,100,"[[1t[gemDescs6]]] ([[2d4*1000]] gp)"]
-]
+const gemDescs3 = {
+  "name": "gemDescs3",
+  "showplayers": false,
+  "simpleEntries": ["amber", "amethyst", "chrysoberyl", "coral", "red garnet", "brown-green garnet", "jade", "jet", "white pearl", "golden pearl", "pink pearl", "silver pearl", "red spinel", "brown spinel", "deep green spinel", "tourmaline"]
+};
+
+const gemDescs4 = {
+  "name": "gemDescs4",
+  "showplayers": false,
+  "simpleEntries": ["alexandrite", "aquamarine", "violet garnet", "black pearl", "deep blue spinel", "golden yellow topaz"]
+};
+
+const gemDescs5 = {
+  "name": "gemDescs5",
+  "showplayers": false,
+  "simpleEntries": ["emerald", "white opal", "black opal", "fire opal", "blue sapphire", "fiery yellow corundum", "rich purple corundum", "blue star sapphire", "black star sapphire", "star ruby"]
+};
+
+const gemDescs6 = {
+  "name": "gemDescs6",
+  "showplayers": false,
+  "simpleEntries": ["clearest bright green emerald", "blue diamond", "white diamond", "canary diamond", "pink diamond", "brown diamond", "blue diamond", "jacinth"]
+};
+
+const basicGems = {
+  "name": "basicGems",
+  "showplayers": false,
+  "entries": [
+    [1,25,"[[1t[gemDescs1]]] ([[4d4]] gp)"],
+    [26,50,"[[1t[gemDescs2]]] ([[2d4*10]] gp)"],
+    [51,70,"[[1t[gemDescs3]]] ([[4d4*10]] gp)"],
+    [71,90,"[[1t[gemDescs4]]] ([[2d4*100]] gp)"],
+    [91,99,"[[1t[gemDescs5]]] ([[4d4*100]] gp)"],
+    [100,100,"[[1t[gemDescs6]]] ([[2d4*1000]] gp)"]
+  ]
+};
 
 
 
 
 on("ready", function() {
-  var tableName, gemDescTable, gemValuesTable, minRoll, maxRoll, valueAndDescriptionRoll, weight;
-  
-  _.each(gemDescriptionsData, function(gemDescLine) {
-    FindOrMakeObjector.setMode("rollabletable");
-    tableName = gemDescLine.shift();
-    gemDescTable = FindOrMakeObjector.findOrMake({"name": tableName, "showplayers": false});
-    FindOrMakeObjector.setMode("tableitem");
-    _.each(gemDescLine.shift(), function(gemDesc) {
-      FindOrMakeObjector.findOrMake({"name": gemDesc, "_rollabletableid": gemDescTable.id});
-    });
+  var gemTables = [
+    gemDescs1,
+    gemDescs2,
+    gemDescs3,
+    gemDescs4,
+    gemDescs5,
+    gemDescs6,
+    basicGems
+  ];
+  _.each(gemTables, function(tableData) {
+    RollableTableManager.findOrMake(tableData);
   });
-
-  FindOrMakeObjector.setMode("rollabletable");
-  gemValuesTable = FindOrMakeObjector.findOrMake({"name": "gems", "showplayers": false});
-  FindOrMakeObjector.setMode("tableitem");
-  _.each(gemData, function(gemDataLine) {
-    [minRoll, maxRoll, valueAndDescriptionRoll] = gemDataLine;
-    weight = (maxRoll - minRoll) + 1;
-    FindOrMakeObjector.findOrMake({"name": valueAndDescriptionRoll, "weight": weight, "_rollabletableid": gemValuesTable.id});
-  })
 });
