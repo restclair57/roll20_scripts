0f8ebb3ebe4dd95f31d5fc5cedabdae56bbe8330
Rob StClair <rob.stclair.57@gmail.com>
2018-06-08 14:48:09 -0400

    Put everything for macros in one file.

--- macros.js
+++ macros.js
@@ -1,6 +1,34 @@
+const desiredMacros = [
+  {
+    "name": "arcanecast",
+    "action": "/em 's fingertips erupt with arcane energy!",
+    "visibleto": "",
+    "istokenaction": false
+  },
+  {
+    "name": "divinecast",
+    "action": "/em 's prayers have been heard!",
+    "visibleto": "",
+    "istokenaction": false
+  },
+  {
+    "name": "duh",
+    "action": "/em rolls [[1d20]] for a DUH check",
+    "visibleto": "all",
+    "istokenaction": false
+  },
+  {
+    "name": "gm-duh",
+    "action": "/emas @{target|token_name} rolls [[1d20]] for a DUH check (by the DM's good graces)",
+    "visibleto": "",
+    "istokenaction": false
+  },
+  
+]
+
 on("ready", function() {
-  FindOrMakeObjector.setMode("macro");
   _.each(desiredMacros, function(dataObj) {
+    FindOrMakeObjector.setMode("macro");
     FindOrMakeObjector.findOrMake(dataObj);
   });
 })
