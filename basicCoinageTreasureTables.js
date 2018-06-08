c7503bfd3d667872016848c4178d2d02860a8337
Rob StClair <rob.stclair.57@gmail.com>
2018-06-07 20:53:29 -0400

    Coinage treasure tables from d20srd. Uses findOrMakeObjector to find-or-make and recursiveTables to actually use.

---
+++ basicCoinageTreasureTables.js
@@ -0,0 +1,17 @@
+on("ready", function() {
+  var tableLevel, minRoll, maxRoll, itemDesc, coinageTableForLevel, tableName, tableData, weight, rollabletableid, itemData;
+  _.each(basicCoinageTreasureTables, function(dataObj) {
+    FindOrMakeObjector.setMode("rollabletable");
+    [tableLevel, minRoll, maxRoll, itemDesc] = dataObj;
+    tableName = "Coinage_" + tableLevel;
+    tableData = {"name": tableName, "showplayers": false};
+    coinageTableForLevel = FindOrMakeObjector.findOrMake(tableData);
+
+    FindOrMakeObjector.setMode("tableitem");
+    weight = maxRoll - minRoll;
+    rollabletableid = coinageTableForLevel.id;
+    itemData = {"_rollabletableid": rollabletableid, "name": itemDesc, "weight": weight}
+    FindOrMakeObjector.findOrMake(itemData);
+  });
+  
+})
