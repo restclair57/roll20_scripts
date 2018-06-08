de47276a9ba50b00c3924002e585fea74306d87e
Rob StClair <rob.stclair.57@gmail.com>
2018-06-08 14:56:25 -0400

    items, gear, weapons, etc tables for treasure

---
+++ basicMundaneItemsTables.js
@@ -0,0 +1,118 @@
+const alchemicalItemsData = {
+  "name": "alchemicalItems",
+  "showplayers": false,
+  "entries": [
+    [1,12,"[[1d4 flasks alchemist’s fire]]"],
+    [13,24,"[[2d4 flasks acid]]"],
+    [25,36,"[[1d4 smokesticks]]"],
+    [37,48,"[[1d4 flasks holy water]]"],
+    [49,62,"[[1d4 doses antitoxin]]"],
+    [63,74,"everburning torch"],
+    [75,88,"[[1d4 tanglefoot bags]]"],
+    [89,100,"[[1d4 thunderstones]]"]
+  ]
+};
+
+const armorItemsData = {
+  "name": "armorItems",
+  "showplayers": false,
+  "entries": [
+    [1,12,"chain shirt"],
+    [13,18,"masterwork studded leather"],
+    [19,26,"breastplate"],
+    [27,34,"banded mail"],
+    [35,54,"half plate"],
+    [55,80,"full plate"],
+    [81,90,"[[1d[darkwoodShieldsItems]]]"],
+    [91,100,"masterwork [[1d[masterworkShieldsItems]]]"]
+  ]
+};
+
+const darkwoodShieldsItemsData = {
+  "name": "darkwoodShieldsItems",
+  "showplayers": false,
+  "entries": [
+    [1,50,"buckler"],
+    [51,100,"shield"]
+  ]
+};
+
+const masterworkShieldsItemsData = {
+  "name": "masterworkShieldsItems",
+  "showplayers": false,
+  "entries": [
+    [1,17,"buckler"],
+    [18,40,"light wooden shield"],
+    [41,60,"light steel shield"],
+    [61,83,"heavy wooden shield"],
+    [84,100,"heavy steel shield"]
+  ]
+};
+
+const weaponsItemsData = {
+  "name": "weaponsItems",
+  "showplayers": false,
+  "entries": [
+    [1,50,"masterwork [[1t[commonMeleeWeaponsItems]]]"],
+    [51,70,"masterwork [[1t[uncommonMeleeWeaponsItems]]]"],
+    [71,100,"masterwork [[1t[commonRangedWeaponsItems]]]"]
+  ]
+};
+
+const toolsAndGearItemsData = {
+  "name": "toolsAndGearItems",
+  "showplayers": false,
+  "entries": [
+    [1,3,"empty backpack"],
+    [4,6,"crowbar"],
+    [7,11,"bullseye lantern"],
+    [12,16,"simple lock"],
+    [17,21,"average lock"],
+    [22,28,"good lock"],
+    [29,35,"superior lock"],
+    [36,40,"masterwork manacles"],
+    [41,43,"small steel mirror"],
+    [44,46,"50 feet of silk rope"],
+    [47,53,"spyglass"],
+    [54,58,"masterwork artisan’s tools"],
+    [59,63,"climber’s kit"],
+    [64,68,"disguise kit"],
+    [69,73,"healer’s kit"],
+    [74,77,"silver holy symbol"],
+    [78,81,"hourglass"],
+    [82,88,"magnifying glass"],
+    [89,95,"masterwork musical instrument"],
+    [96,100,"masterwork thieves’ tools"]
+  ]
+};
+
+const mundaneItemsData = {
+  "name": "mundaneItems",
+  "showplayers": false,
+  "entries": [
+    [1,17,"[[1t[alchemicalItems]]]"],
+    [18,21,"small [[1t[armorItems]]]"],
+    [22,50,"medium [[1t[armorItems]]]"],
+    [51,83,"[[1t[weaponsItems]]]"],
+    [84,100,"[[1t[toolsAndGearItems]]]"]
+  ]
+};
+
+on("ready", function() {
+  var basicMundaneItemsTables = [
+    alchemicalItemsData,
+    armorItemsData,
+    darkwoodShieldsItemsData,
+    masterworkShieldsItemsData,
+    weaponsItemsData,
+    toolsAndGearItemsData,
+    mundaneItemsData
+  ];
+  _.each(basicMundaneItemsTables, function(tableData) {
+    RollableTableManager.findOrMake(tableData);
+  });
+});
+
+
+
+
