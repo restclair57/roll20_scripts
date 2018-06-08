a4b3b43c6ad5622caa5d37740a3ca80674a7dfc1
Rob StClair <rob.stclair.57@gmail.com>
2018-06-07 20:53:54 -0400

    Use findOrMakeObjector to simplify

--- macros.js
+++ macros.js
@@ -1,15 +1,6 @@
 on("ready", function() {
-  const robsPlayerId = RobsPlayerId.get();
+  FindOrMakeObjector.setMode("macro");
   _.each(desiredMacros, function(dataObj) {
-    var toFind = Object.assign({"_type": "macro"}, dataObj);
-    var matchedMacros = findObjs(toFind);
-    if(_.isEmpty(matchedMacros)) {
-      log("creating macro named '" + dataObj["name"] + "'");
-      var toCreate = Object.assign({"playerid": robsPlayerId}, dataObj);
-      createObj("macro", toCreate);
-    } else {
-      log("found macro named '" + dataObj["name"] + "'");
-    };
+    FindOrMakeObjector.findOrMake(dataObj);
   });
-  
 })
