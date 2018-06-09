f7eca9d003759e6e0f6a79a9f3126264ca637d7f
Rob StClair <rob.stclair.57@gmail.com>
2018-06-09 09:15:40 -0400

    Add melee/ranged distinction. Add bane targets.

--- magicWeaponsTables.js
+++ magicWeaponsTables.js
@@ -67,11 +67,214 @@ const majorMagicWeapons = {
   ]
 };
 
+
+
+
+const minorMagicMeleeWeaponAbilities = {
+  "name": "minorMagicMeleeWeaponAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,10,"Bane ([[1t[baneTargets]]])"],
+    [11,17,"Defending"],
+    [18,27,"Flaming"],
+    [28,37,"Frost"],
+    [38,47,"Shock"],
+    [48,56,"Ghost touch"],
+    [57,67,"Keen"],
+    [68,71,"Ki Focus"],
+    [72,75,"Merciful"],
+    [76,82,"Mighty cleaving"],
+    [83,87,"Spell storing"],
+    [88,91,"Throwing"],
+    [92,95,"Thundering"],
+    [96,99,"Vicious"],
+    [100,100,"[[1t[minorMagicMeleeWeaponAbilities]]] [[1t[minorMagicMeleeWeaponAbilities]]]"]
+  ]
+};
+
+const mediumMagicMeleeWeaponAbilities = {
+  "name": "mediumMagicMeleeWeaponAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,6,"Bane ([[1t[baneTargets]]])"],
+    [7,12,"Defending"],
+    [13,19,"Flaming"],
+    [20,26,"Frost"],
+    [27,33,"Shock"],
+    [34,38,"Ghost touch"],
+    [39,44,"Keen"],
+    [45,48,"Ki Focus"],
+    [49,50,"Merciful"],
+    [51,54,"Mighty cleaving"],
+    [55,59,"Spell storing"],
+    [60,63,"Throwing"],
+    [64,65,"Thundering"],
+    [66,69,"Vicious"],
+    [70,72,"Anarchic"],
+    [73,75,"Axiomatic"],
+    [76,78,"Disruption"],
+    [79,81,"Flaming burst"],
+    [82,84,"Icy burst"],
+    [85,87,"Holy"],
+    [88,90,"Shocking burst"],
+    [91,93,"Unholy"],
+    [94,95,"Wounding"],
+    [96,100,"[[1t[mediumMagicMeleeWeaponAbilities]]] [[1t[mediumMagicMeleeWeaponAbilities]]]"]
+  ]
+};
+
+const majorMagicMeleeWeaponAbilities = {
+  "name": "majorMagicMeleeWeaponAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,3,"Bane ([[1t[baneTargets]]])"],
+    [4,6,"Flaming"],
+    [7,9,"Frost"],
+    [10,12,"Shock"],
+    [13,15,"Ghost touch"],
+    [16,19,"Ki Focus"],
+    [20,21,"Mighty cleaving"],
+    [22,24,"Spell storing"],
+    [25,28,"Throwing"],
+    [29,32,"Thundering"],
+    [33,36,"Vicious"],
+    [37,41,"Anarchic"],
+    [42,46,"Axiomatic"],
+    [47,49,"Disruption"],
+    [50,54,"Flaming burst"],
+    [55,59,"Icy burst"],
+    [60,64,"Holy"],
+    [65,69,"Shocking burst"],
+    [70,74,"Unholy"],
+    [75,78,"Wounding"],
+    [79,83,"Speed"],
+    [84,86,"Brilliant energy"],
+    [87,88,"Dancing"],
+    [89,90,"Vorpal"],
+    [91,100,"[[1t[majorMagicMeleeWeaponAbilities]]] [[1t[majorMagicMeleeWeaponAbilities]]]"]
+  ]
+};
+
+
+
+const minorMagicRangedWeaponAbilities = {
+  "name": "minorMagicRangedWeaponAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,12,"Bane ([[1t[baneTargets]]])"],
+    [13,25,"Distance"],
+    [26,40,"Flaming"],
+    [41,55,"Frost"],
+    [56,60,"Merciful"],
+    [61,68,"Returning"],
+    [69,83,"Shock"],
+    [84,93,"Seeking"],
+    [94,99,"Thundering"],
+    [100,100,"[[1t[minorMagicRangedWeaponAbilities]]] [[1t[minorMagicRangedWeaponAbilities]]]"]
+  ]
+};
+
+const mediumMagicRangedWeaponsAbilities = {
+  "name": "mediumMagicRangedWeaponsAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,8,"Bane ([[1t[baneTargets]]])"],
+    [9,16,"Distance"],
+    [17,28,"Flaming"],
+    [29,40,"Frost"],
+    [41,42,"Merciful"],
+    [43,47,"Returning"],
+    [48,59,"Shock"],
+    [60,64,"Seeking"],
+    [65,68,"Thundering"],
+    [69,71,"Anarchic"],
+    [72,74,"Axiomatic"],
+    [75,79,"Flaming burst"],
+    [80,82,"Holy"],
+    [83,87,"Icy burst"],
+    [88,92,"Shocking burst"],
+    [93,95,"Unholy"],
+    [96,100,"[[1t[mediumMagicRangedWeaponsAbilities]]] [[1t[mediumMagicRangedWeaponsAbilities]]]"]
+  ]
+};
+
+const majorMagicRangedWeaponsAbilities = {
+  "name": "majorMagicRangedWeaponsAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,4,"Bane ([[1t[baneTargets]]])"],
+    [5,8,"Distance"],
+    [9,12,"Flaming"],
+    [13,16,"Frost"],
+    [17,21,"Returning"],
+    [22,25,"Shock"],
+    [26,27,"Seeking"],
+    [28,29,"Thundering"],
+    [30,34,"Anarchic"],
+    [35,39,"Axiomatic"],
+    [40,49,"Flaming burst"],
+    [50,54,"Holy"],
+    [55,64,"Icy burst"],
+    [65,74,"Shocking burst"],
+    [75,79,"Unholy"],
+    [80,84,"Speed"],
+    [85,90,"Brilliant energy"],
+    [91,100,"[[1t[majorMagicWeaponAbilities]]] [[1t[majorMagicWeaponAbilities]]]"]
+  ]
+};
+
+
+const baneTargets = {
+  "name": "baneTargets",
+  "showplayers": false,
+  "entries": [
+    [1,5,"Aberrations"],
+    [6,9,"Animals"],
+    [10,16,"Constructs"],
+    [17,22,"Dragons"],
+    [23,27,"Elementals"],
+    [28,32,"Fey"],
+    [33,39,"Giants"],
+    [40,40,"Humanoids, aquatic"],
+    [41,42,"Humanoids, dwarf"],
+    [43,44,"Humanoids, elf"],
+    [45,45,"Humanoids, gnoll"],
+    [46,46,"Humanoids, gnome"],
+    [47,49,"Humanoids, goblinoid"],
+    [50,50,"Humanoids, halfling"],
+    [51,54,"Humanoids, human"],
+    [55,57,"Humanoids, reptilian"],
+    [58,60,"Humanoids, orc"],
+    [61,65,"Magical beasts"],
+    [66,70,"Monstrous humanoids"],
+    [71,72,"Oozes"],
+    [73,73,"Outsiders, air"],
+    [74,76,"Outsiders, chaotic"],
+    [77,77,"Outsiders, earth"],
+    [78,80,"Outsiders, evil"],
+    [81,81,"Outsiders, fire"],
+    [82,84,"Outsiders, good"],
+    [85,87,"Outsiders, lawful"],
+    [88,88,"Outsiders, water"],
+    [89,90,"Plants"],
+    [91,98,"Undead"],
+    [99,100,"Vermin"]
+  ]
+};
+
+
 on("ready", function() {
   var specificMagicWeapons = [
     minorMagicWeapons,
     mediumMagicWeapons,
-    majorMagicWeapons
+    majorMagicWeapons,
+    minorMagicRangedWeaponAbilities,
+    mediumMagicRangedWeaponsAbilities,
+    majorMagicRangedWeaponsAbilities,
+    minorMagicMeleeWeaponAbilities,
+    mediumMagicMeleeWeaponAbilities,
+    majorMagicMeleeWeaponAbilities,
+    baneTargets
   ];
   _.each(specificMagicWeapons, function(tableData) {
     RollableTableManager.findOrMake(tableData);
