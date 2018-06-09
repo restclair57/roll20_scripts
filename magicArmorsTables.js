880af4968b47541706ef6f14e4b85137cf8ae800
Rob StClair <rob.stclair.57@gmail.com>
2018-06-09 09:54:14 -0400

    Add magic armor/shield abilities.

--- magicArmorsTables.js
+++ magicArmorsTables.js
@@ -79,6 +79,170 @@ const majorMagicShields = {
 
 
 
+const minorMagicArmorAbilities = {
+  "name": "minorMagicArmorAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,25,"Glamered"],
+    [26,32,"Fortification, light"],
+    [33,52,"Slick"],
+    [53,72,"Shadow"],
+    [73,92,"Silent moves"],
+    [93,96,"Spell resistance (13)"],
+    [97,97,"Slick, improved"],
+    [98,98,"Shadow, improved"],
+    [99,99,"Silent moves, improved"],
+    [100,100,"[[1t[minorMagicArmorAbilities]]] [[1t[minorMagicArmorAbilities]]]"]
+  ]
+};
+
+const mediumMagicArmorAbilities = {
+  "name": "mediumMagicArmorAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,5,"Glamered"],
+    [6,8,"Fortification, light"],
+    [9,11,"Slick"],
+    [12,14,"Shadow"],
+    [15,17,"Silent moves"],
+    [18,19,"Spell resistance (13)"],
+    [20,29,"Slick, improved"],
+    [30,39,"Shadow, improved"],
+    [40,49,"Silent moves, improved"],
+    [50,54,"Acid resistance"],
+    [55,59,"Cold resistance"],
+    [60,64,"Electricity resistance"],
+    [65,69,"Fire resistance"],
+    [70,74,"Sonic resistance"],
+    [75,79,"Ghost touch"],
+    [80,84,"Invulnerability"],
+    [85,89,"Fortification, moderate"],
+    [90,94,"Spell resistance (15)"],
+    [95,99,"Wild"],
+    [100,100,"[[1t[mediumMagicArmorAbilities]]] [[1t[mediumMagicArmorAbilities]]]"]
+  ]
+};
+
+const majorMagicArmorAbilities = {
+  "name": "majorMagicArmorAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,3,"Glamered"],
+    [4,4,"Fortification, light"],
+    [5,7,"Slick, improved"],
+    [8,10,"Shadow, improved"],
+    [11,13,"Silent moves, improved"],
+    [14,16,"Acid resistance"],
+    [17,19,"Cold resistance"],
+    [20,22,"Electricity resistance"],
+    [23,25,"Fire resistance"],
+    [26,28,"Sonic resistance"],
+    [29,33,"Ghost touch"],
+    [34,35,"Invulnerability"],
+    [36,40,"Fortification, moderate"],
+    [41,42,"Spell resistance (15)"],
+    [43,43,"Wild"],
+    [44,48,"Slick, greater"],
+    [49,53,"Shadow, greater"],
+    [54,58,"Silent moves, greater"],
+    [59,63,"Acid resistance, improved"],
+    [64,68,"Cold resistance, improved"],
+    [69,73,"Electricity resistance, improved"],
+    [74,78,"Fire resistance, improved"],
+    [79,83,"Sonic resistance, improved"],
+    [84,88,"Spell resistance (17)"],
+    [89,89,"Etherealness"],
+    [90,90,"Undead controlling"],
+    [91,92,"Fortification, heavy"],
+    [93,94,"Spell resistance (19)"],
+    [95,95,"Acid resistance, greater"],
+    [96,96,"Cold resistance, greater"],
+    [97,97,"Electricity resistance, greater"],
+    [98,98,"Fire resistance, greater"],
+    [99,99,"Sonic resistance, greater"],
+    [100,100,"[[1t[majorMagicArmorAbilities]]] [[1t[majorMagicArmorAbilities]]]"]
+  ]
+};
+
+
+const minorMagicShieldAbilities = {
+  "name": "minorMagicShieldAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,20,"Arrow catching"],
+    [21,40,"Bashing"],
+    [41,50,"Blinding"],
+    [51,75,"Fortification, light"],
+    [76,92,"Arrow deflection"],
+    [93,97,"Animated"],
+    [98,99,"Spell resistance (13)"],
+    [100,100,"[[1t[minorMagicShieldAbilities]]] [[1t[minorMagicShieldAbilities]]]"]
+  ]
+};
+
+const mediumMagicShieldAbilities = {
+  "name": "mediumMagicShieldAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,10,"Arrow catching"],
+    [11,20,"Bashing"],
+    [21,25,"Blinding"],
+    [26,40,"Fortification, light"],
+    [41,50,"Arrow deflection"],
+    [51,57,"Animated"],
+    [58,59,"Spell resistance (13)"],
+    [60,63,"Acid resistance"],
+    [64,67,"Cold resistance"],
+    [68,71,"Electricity resistance"],
+    [72,75,"Fire resistance"],
+    [76,79,"Sonic resistance"],
+    [80,85,"Ghost touch"],
+    [86,95,"Fortification, moderate"],
+    [96,98,"Spell resistance (15)"],
+    [99,99,"Wild"],
+    [100,100,"[1t[mediumMagicShieldAbilities]]] [[1t[mediumMagicShieldAbilities]]]"]
+  ]
+};
+
+const majorMagicShieldAbilities = {
+  "name": "majorMagicShieldAbilities",
+  "showplayers": false,
+  "entries": [
+    [1,5,"Arrow catching"],
+    [6,8,"Bashing"],
+    [9,10,"Blinding"],
+    [11,15,"Fortification, light"],
+    [16,20,"Arrow deflection"],
+    [21,25,"Animated"],
+    [26,28,"Acid resistance"],
+    [29,31,"Cold resistance"],
+    [32,34,"Electricity resistance"],
+    [35,37,"Fire resistance"],
+    [38,40,"Sonic resistance"],
+    [41,46,"Ghost touch"],
+    [47,56,"Fortification, moderate"],
+    [57,58,"Spell resistance (15)"],
+    [59,59,"Wild"],
+    [60,64,"Acid resistance, improved"],
+    [65,69,"Cold resistance, improved"],
+    [70,74,"Electricity resistance, improved"],
+    [75,79,"Fire resistance, improved"],
+    [80,84,"Sonic resistance, improved"],
+    [85,86,"Spell resistance (17)"],
+    [87,87,"Undead controlling"],
+    [88,91,"Fortification, heavy"],
+    [92,93,"Reflecting"],
+    [94,94,"Spell resistance (19)"],
+    [95,95,"Acid resistance, greater"],
+    [96,96,"Cold resistance, greater"],
+    [97,97,"Electricity resistance, greater"],
+    [98,98,"Fire resistance, greater"],
+    [99,99,"Sonic resistance, greater"],
+    [100,100,"[1t[majorMagicShieldAbilities]]] [[1t[majorMagicShieldAbilities]]]"]
+  ]
+};
+
+
 
 on("ready", function() {
   var specificMagicArmors = [
@@ -87,7 +251,13 @@ on("ready", function() {
     majorMagicArmors,
     minorMagicShields,
     mediumMagicShields,
-    majorMagicShields
+    majorMagicShields,
+    minorMagicArmorAbilities,
+    mediumMagicArmorAbilities,
+    majorMagicArmorAbilities,
+    minorMagicShieldAbilities,
+    mediumMagicShieldAbilities,
+    majorMagicShieldAbilities
   ];
   _.each(specificMagicArmors, function(tableData) {
     RollableTableManager.findOrMake(tableData);
